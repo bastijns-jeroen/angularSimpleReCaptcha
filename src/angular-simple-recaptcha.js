440d3a95ec3bd15cf08336f2c2e7bfac8d987449
Bert De Wolf <bert.dewolf@aca-it.be>
2016-10-25 15:15:17 +0200

    Update readme

--- src/angular-simple-recaptcha.js
+++ src/angular-simple-recaptcha.js
@@ -1,66 +1,66 @@
 (function() {
-    "use strict";
-    //Use an IIFE
+	"use strict";
+	//Use an IIFE
 
-    function simpleRecaptchaDirective() {
-        var directiveDataObject =
-        {
-            restrict: 'AE',
-            scope: {
-              siteKey: '@',
+	function simpleRecaptchaDirective() {
+		var directiveDataObject =
+		{
+			restrict: 'AE',
+			scope: {
+			  siteKey: '@',
 			  captchaReady: '&'
-            },
-            require: 'ngModel',
-            link: link,
-        };
+			},
+			require: 'ngModel',
+			link: link,
+		};
 
-        return directiveDataObject;
+		return directiveDataObject;
 
-        function link(scope, elm, attrs, ngModel) {
-            var id;
-            ngModel.$validators.captcha = function(modelValue, ViewValue) {
-                // if the viewvalue is empty, there is no response yet,
-                // so we need to raise an error.
-                return !!ViewValue;
-            };
+		function link(scope, elm, attrs, ngModel) {
+			var id;
+			ngModel.$validators.captcha = function(modelValue, ViewValue) {
+				// if the viewvalue is empty, there is no response yet,
+				// so we need to raise an error.
+				return !!ViewValue;
+			};
 
-            function update(response) {
-                ngModel.$setViewValue(response);
-                ngModel.$render();
-            }
+			function update(response) {
+				ngModel.$setViewValue(response);
+				ngModel.$render();
+			}
 
-            function expired() {
-                grecaptcha.reset(id);
-                ngModel.$setViewValue('');
-                ngModel.$render();
-                // do an apply to make sure the  empty response is
-                // proaganded into your models/view.
-                // not really needed in most cases tough! so commented by default
-                // scope.$apply();
-            }
+			function expired() {
+				grecaptcha.reset(id);
+				ngModel.$setViewValue('');
+				ngModel.$render();
+				// do an apply to make sure the  empty response is
+				// proaganded into your models/view.
+				// not really needed in most cases tough! so commented by default
+				// scope.$apply();
+			}
 
 			function resetCaptcha() {
 				grecaptcha.reset(id);
 			}
 
-            function iscaptchaReady() {
-                if (typeof grecaptcha !== "object") {
-                    // api not yet ready, retry in a while
-                    return setTimeout(iscaptchaReady, 250);
-                }
-                id = grecaptcha.render(
-                    elm[0], {
-                        "sitekey": scope.siteKey,
-                        "callback": update,
-                        "expired-callback": expired
-                    }
-                );
+			function iscaptchaReady() {
+				if (typeof grecaptcha !== "object") {
+					// api not yet ready, retry in a while
+					return setTimeout(iscaptchaReady, 250);
+				}
+				id = grecaptcha.render(
+					elm[0], {
+						"sitekey": scope.siteKey,
+						"callback": update,
+						"expired-callback": expired
+					}
+				);
 				scope.captchaReady({ reset: resetCaptcha });
-            }
-            iscaptchaReady();
-        }
-    }
+			}
+			iscaptchaReady();
+		}
+	}
 
-    angular.module('AngularSimpleRecaptcha', [])
-    .directive('simpleRecaptcha', simpleRecaptchaDirective);
+	angular.module('AngularSimpleRecaptcha', [])
+	.directive('simpleRecaptcha', simpleRecaptchaDirective);
 }());
